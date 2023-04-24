61f52bfb95cbe7839e8dba072a276d19d3c2e778
김진호 <wlsghsla94@naver.com>
2023-04-24 17:17:04 +0900

    0424

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,32 +1,9 @@
 package hello.hellospring.service;
 
-import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemberRepository() {
-        @Override
-        public Member save(Member member) {
-            return null;
-        }
-
-        @Override
-        public Optional<Member> findById(Long id) {
-            return Optional.empty();
-        }
-
-        @Override
-        public Optional<Member> findByName(String name) {
-            return Optional.empty();
-        }
-
-        @Override
-        public List<Member> findAll() {
-            return null;
-        }
     }
 }
